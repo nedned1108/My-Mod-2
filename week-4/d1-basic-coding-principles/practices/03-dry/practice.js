2e62fde9b1bcbe631891fcbce2255a76c621500b
nednguyen <rpmshockwave@gmail.com>
2022-10-18 12:12:34 -0400

    w4-d2-01-common-js

--- week-4/d1-basic-coding-principles/practices/03-dry/practice.js
+++ week-4/d1-basic-coding-principles/practices/03-dry/practice.js
@@ -1,47 +1,71 @@
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+
+  return bigger(num1, num2) * 2;
+  // if (num1 > num2) {
+  //   bigNum = num1;
+  //   return bigNum * 2;
+  // } else {
+  //   bigNum = num2;
+  //   return bigNum * 2;
+  // }
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return bigger(num1, num2) / 3;
+  // let bigNum;
+  // if (num1 > num2) {
+  //   bigNum = num1;
+  //   return bigNum / 3;
+  // } else {
+  //   bigNum = num2;
+  //   return bigNum / 3;
+  // }
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  return `I ate ${bigger(sum1, sum2)} tacos.`;
+  // let bigNum;
+  // if (sum1 > sum2) {
+  //   bigNum = sum1;
+  //   return `I ate ${bigNum} tacos.`;
+  // } else {
+  //   bigNum = sum2;
+  //   return `I ate ${bigNum} tacos.`;
+  // }
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
-}
 
+  let smallDog = bigger(weight1, weight2) === weight1 ? weight2 : weight1;
+  return `I adopted a dog that weighs ${smallDog} pounds.`;
+  // let smallDog;
+  // if (weight1 < weight2) {
+  //   smallDog = weight1;
+  //   return `I adopted a dog that weighs ${smallDog} pounds.`;
+  // } else {
+  //   smallDog = weight2;
+  //   return `I adopted a dog that weighs ${smallDog} pounds.`;
+  // }
+}
+// function smaller(el1, el2) {
+//   let big;
+//   if (el1 < el2) {
+//     big = el1;
+//   } else {
+//     big = el2;
+//   }
+//   return big;
+// }
+
+function bigger(el1, el2) {
+  let big;
+  if (el1 > el2) {
+    big = el1;
+  } else {
+    big = el2;
+  }
+  return big;
+}
 
 /**************************************************************************/
 /* DO NOT CHANGE THE CODE BELOW */
--- week-4/d2-es6-classes/practices/01-common-js-modules/index.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/index.js
@@ -1,6 +1,13 @@
 
 // Your code here
 
+const {message1, message2, message3} = require('./messages');
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
+const sayHelloTo = require('./send-messages/say-hello-to')
+msg1 = message1;
+msg2 = message2;
+msg3 = message3;
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
